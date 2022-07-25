0d3daeab17c3ef63df10b5a4cb46c0077531b4a7
freemt <yucongo+fmt@gmail.com>
2022-07-26 00:20:18 +0800

    Update 0.1.0a2

--- get_pwbrowser_sync/__init__.py
+++ get_pwbrowser_sync/__init__.py
@@ -1,5 +1,5 @@
 """Init."""
-__version__ = "0.1.0a1"
+__version__ = "0.1.0a2"
 from .get_pwbrowser_sync import get_pwbrowser_sync
 
 # from .get_pwbrowser_async import get_pwbrowser as get_pwbrowser_async
